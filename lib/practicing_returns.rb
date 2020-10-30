ee91144ee6e0a89245955d5c7b254ddde0e5a323
Dillon Nguyen <dillon.d.nguyen@gmail.com>
2020-10-30 11:39:38 -0700

    Done.

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,12 +1,24 @@
-require 'pry'
+# require 'pry'
+
+# def hello(array)
+#   i = 0
+#   while i < array.length
+#     yield(array[i])
+#     i += 1
+#   end
+# end
+
+
+# hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
 
 def hello(array)
   i = 0
+  collection = []
   while i < array.length
-    yield(array[i])
+    collection << yield(array[i])
     i += 1
   end
-end
+  collection
+end 
 
-
-hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
+'hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
